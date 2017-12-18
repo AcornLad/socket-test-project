7cfce4df747d7cec15ce906ed19c0eeaff6b8a09
Conrad Cash (Lake Mary) <conrad.cash@superion.com>
2017-12-18 15:47:14 -0500

    Implemented socket.io

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req,res){
   res.sendFile(__dirname + '/index.html');
+});
+
+//Whenever someone connects, this is executed
+io.on('connection', function(socket){
+  console.log('User connected');
+
+  //Whenever someone disconnects, this is executed
+  socket.on('disconnect', function(){
+    console.log('User disconnected');
+  });
 });
 
 http.listen(3000, function(){
